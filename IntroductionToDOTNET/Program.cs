711ed8236b2eea1077a5c0f72bcad8f387cd3339
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-10-18 18:16:07 +0300

    Geometry DONE

--- IntroductionToDOTNET/Program.cs
+++ IntroductionToDOTNET/Program.cs
@@ -1,4 +1,6 @@
 ﻿//#define CONSOLE
+//#dfeine IO
+#define GEOMETRY
 
 using System;
 using System.Collections.Generic;
@@ -27,6 +29,7 @@ namespace IntroductionToDOTNET
             Console.ResetColor(); 
 #endif
 
+#if IO
 			Console.Write("Введите ваше имя: ");
 			string first_name = Console.ReadLine();
 			Console.Write("Введите вашу фамилию: ");
@@ -35,7 +38,63 @@ namespace IntroductionToDOTNET
 			int age = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine(last_name + " " + first_name + " " + age);
 			Console.WriteLine(String.Format("{0} {1} {2}", last_name, first_name, age));
-			Console.WriteLine($"{last_name} {first_name} {age}");	// Интерполяция строк
+			Console.WriteLine($"{last_name} {first_name} {age}");   // Интерполяция строк  
+#endif
+
+#if GEOMETRY
+			Console.Write("Введите число: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j) Console.Write("* ");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j) Console.Write(j <= i ? "* " : "");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j) Console.Write(j >= i ? "* " : "");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j) Console.Write(j >= i ? "* " : "  ");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j) Console.Write(j >= n - i - 1 ? "* " : "  ");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n - i - 1; ++j) Console.Write(" ");
+				Console.Write("/");
+				for (int j = 0; j < i * 2; j++) Console.Write(" ");
+				Console.WriteLine("\\");
+			}
+			for (int i = n - 1; i >= 0; --i)
+			{
+				for (int j = 0; j < n - i - 1; ++j) Console.Write(" ");
+				Console.Write("\\");
+				for (int j = 0; j < i * 2; ++j) Console.Write(" ");
+				Console.WriteLine("/");
+			}
+			Console.WriteLine();
+			for (int i = 0; i < n; ++i)
+			{
+				for (int j = 0; j < n; ++j) Console.Write(i % 2 == 0 ? (j % 2 == 0 ? "+ " : "- ") : (j % 2 != 0 ? "+ " : "- "));
+				Console.WriteLine();
+			}
+#endif
 		}
 	}
 }
