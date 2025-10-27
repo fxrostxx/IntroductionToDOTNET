2654dfe97c1bc3609f012d7418794a1faee03470
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-10-27 21:42:50 +0300

    Calc in progress

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//#define CALC_IF
+//#define CALC_SWITCH
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,27 +13,44 @@ namespace Calc
 	{
 		static void Main(string[] args)
 		{
-			while (true)
+			//while (true)
 			{
-				Console.Write("Введите выражение (exit для выхода): ");
-				string expr = Console.ReadLine();
-				if (expr == "exit") break;
-				string[] parts = expr.Split(' ');
-				try
+				Console.Write("Введите арифметическое выражение: ");
+				//string expr = Console.ReadLine();
+				string expr = "22+33 - 44 / 2+8* 3";
+				expr = expr.Replace(" ", "");
+				Console.WriteLine(expr);
+				char[] operators = new char[] { '+', '-', '*', '/' };
+				string[] operands = expr.Split(operators);
+				double[] values = new double[operands.Length];
+				char[] digits = "0123456789.".ToCharArray();
+				string[] operations = expr.Split(digits);
+				operations = operations.Where(operation => operation != "").ToArray();
+				for (int i = 0; i < operands.Length; ++i)
 				{
-					switch (Convert.ToChar(parts[1]))
-					{
-						case '+': Console.WriteLine(Convert.ToDouble(parts[0]) + Convert.ToDouble(parts[2])); break;
-						case '-': Console.WriteLine(Convert.ToDouble(parts[0]) - Convert.ToDouble(parts[2])); break;
-						case '*': Console.WriteLine(Convert.ToDouble(parts[0]) * Convert.ToDouble(parts[2])); break;
-						case '/': Console.WriteLine(Convert.ToDouble(parts[0]) / Convert.ToDouble(parts[2])); break;
-						default: Console.WriteLine("Ошибка: неизвестный оператор"); break;
-					}
+					values[i] = Convert.ToDouble(operands[i]);
+					Console.Write($"{values[i]}\t");
 				}
-				catch (Exception e)
+				Console.WriteLine();
+				for (int i = 0; i < operations.Length; ++i) Console.Write($"{operations[i]}\t");
+				Console.WriteLine();
+#if CALC_IF
+				if (expr.Contains("+")) Console.WriteLine($"{values[0]} + {values[1]} = {values[0] + values[1]}");
+				else if (expr.Contains("-")) Console.WriteLine($"{values[0]} - {values[1]} = {values[0] - values[1]}");
+				else if (expr.Contains("*")) Console.WriteLine($"{values[0]} * {values[1]} = {values[0] * values[1]}");
+				else if (expr.Contains("/")) Console.WriteLine($"{values[0]} / {values[1]} = {values[0] / values[1]}");
+				else Console.WriteLine("Error"); 
+#endif
+#if CALC_SWITCH
+				switch (expr[expr.IndexOfAny(operators)])
 				{
-					Console.WriteLine("Ошибка: проверьте корректность выражения");
-				}
+					case '+': Console.WriteLine($"{values[0]} + {values[1]} = {values[0] + values[1]}"); break;
+					case '-': Console.WriteLine($"{values[0]} - {values[1]} = {values[0] - values[1]}"); break;
+					case '*': Console.WriteLine($"{values[0]} * {values[1]} = {values[0] * values[1]}"); break;
+					case '/': Console.WriteLine($"{values[0]} / {values[1]} = {values[0] / values[1]}"); break;
+				} 
+#endif
+
 			}
 		}
 	}
