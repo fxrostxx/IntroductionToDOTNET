2fa4db0ff52c4f89698d40e4e495300fa8cc8c9e
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-10-29 20:18:41 +0300

    Multiplication and Division DONE

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -17,7 +17,8 @@ namespace Calc
 			{
 				Console.Write("Введите арифметическое выражение: ");
 				//string expr = Console.ReadLine();
-				string expr = "22+33 - 44 / 2+8* 3";
+				//string expr = "22+33 - 44 / 2+8* 3";
+				string expr = "22*33 / 44 / 2*8* 3";
 				expr = expr.Replace(" ", "");
 				Console.WriteLine(expr);
 				char[] operators = new char[] { '+', '-', '*', '/' };
@@ -34,6 +35,24 @@ namespace Calc
 				Console.WriteLine();
 				for (int i = 0; i < operations.Length; ++i) Console.Write($"{operations[i]}\t");
 				Console.WriteLine();
+				while (operations[0] != "")
+				{
+					int i = 0;
+					for (; i < operations.Length; ++i)
+					{
+						if (operations[i] == "*" || operations[i] == "/")
+						{
+							if (operations[i] == "*") values[i] *= values[i + 1];
+							else values[i] /= values[i + 1];
+						}
+						for (int index = i; index < operations.Length - 1; ++index) operations[index] = operations[index + 1];
+						for (int index = i + 1; index < values.Length - 1; ++index) values[index] = values[index + 1];
+						operations[operations.Length - 1] = "";
+						values[values.Length - 1] = 0;
+						if (operations[i] == "*" || operations[i] == "/") --i;
+					}
+				}
+				Console.WriteLine(values[0]);
 #if CALC_IF
 				if (expr.Contains("+")) Console.WriteLine($"{values[0]} + {values[1]} = {values[0] + values[1]}");
 				else if (expr.Contains("-")) Console.WriteLine($"{values[0]} - {values[1]} = {values[0] - values[1]}");
@@ -50,8 +69,12 @@ namespace Calc
 					case '/': Console.WriteLine($"{values[0]} / {values[1]} = {values[0] / values[1]}"); break;
 				} 
 #endif
-
 			}
+		}
+		static void Shift(object[] arr, int index)
+		{
+			for (int i = index; i < arr.Length; ++i) arr[i] = arr[i + 1];
+			arr[arr.Length - 1] = new object();
 		}
 	}
 }
